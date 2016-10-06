519612ab18794a54c05efdd4fd4e575855b5dd02
Anand Kumar Singh <anandkumarsingh808@gmail.com>
2016-10-06 23:24:02 +0530

    Add files via upload

--- src/com/seed/DisplayCartDetailsServlet.java
+++ src/com/seed/DisplayCartDetailsServlet.java
@@ -2,9 +2,13 @@ package com.seed;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +20,7 @@ import com.seed.util.ProductsRepository;
 import com.seed.util.ProductsRepositoryInMemoryImpl;
 
 //TODO:0	Modification required
+@WebServlet("/displayDetails")
 public class DisplayCartDetailsServlet extends HttpServlet {
 	private Map<Integer, Product> productEntries;
 	
@@ -41,7 +46,10 @@ public class DisplayCartDetailsServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		ShoppingCart cartRef = null;
-
+		
+		ArrayList<Product> products = null;
+		Set<Integer>  ids = null;
+		
 //		Don't create a new session, if it is already expired
 		HttpSession session = request.getSession(false);
 
@@ -53,17 +61,43 @@ public class DisplayCartDetailsServlet extends HttpServlet {
 		
 //		TODO:1	Retrieve attribute named as "shoppingCart" from session scope,
 //		and assign it to "cartRef" local variable
-
+		cartRef = (ShoppingCart) session.getAttribute("shoppingCart");
 
 //		TODO:2 If the attribute(shoppingCart) exists, retrieve product ids from it.
+		
+		if(cartRef!=null){
+			ids = cartRef.getProductSet();
+		}
 		
 //		TODO:3 If the attribute(shoppingCart) exists, retrieve product ids from it.
 		
 //		TODO:4	Scan all productids retrieved from Cart and maintain a list of corresponding products	
 //		Note: You can get details of product(value) based on productid(key) 
-//		using member variable "productEntries"		
-			
+//		using member variable "productEntries"
+		if(ids!=null){
+			products = new ArrayList<Product>();
+			for(Integer id : ids){
+				products.add(productEntries.get(id));
+			}
+		}
+		
+		
 //		TODO:5	display product details in tabular format as HTTP response(text/html) to the web-client
+		
+		/*out.println("<html><body>");
+		out.println("<table border=1>");
+		out.println("<th>Book Name</th>");
+		
+		for(String s: products){
+			out.println("<tr>");
+			out.println("<td>"+s+"</td>");
+			out.println("</tr>");
+		}
+		
+		out.println("</table>");
+		out.println("<html><body>");*/
+		
+		response.sendRedirect("displayDetails");
 			
 	}
 
